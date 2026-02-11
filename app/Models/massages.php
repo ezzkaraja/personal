eba44456865fc06a63e6bf6fa8f2b9e982674e78
ezzkaraja <129699099+ezzkaraja@users.noreply.github.com>
2026-02-12 02:38:25 +0300

    Fix registration mobile issue

---
+++ app/Models/massages.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class massages extends Model
+{
+    //
+   protected $guarded = [];
+}
---
+++ database/migrations/2026_02_06_000816_create_education_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->id();
+            $table->string('college');
+            $table->string('location');
+            $table->string('degree');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('education');
+    }
+};
